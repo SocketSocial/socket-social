78ebb838f83d39bddeb11309aa0ae3d7127a1d34
Connor Bryan <cbryan@dealersocket.com>
2016-09-21 14:52:30 -0500

    Added placeholders for API: User, Events, Hobbies

--- api/api.js
+++ api/api.js
@@ -4,6 +4,8 @@ const md5 = require('md5');
 
 module.exports = function (app, sequelize, models) {
 
+    // === U S E R S === //
+
     // Sign in to the application
     app.post('/signin', (req, res) => {
        const email    = req.body.email;
@@ -19,12 +21,10 @@ module.exports = function (app, sequelize, models) {
                     res.send({ 'success': true, user });
                 } else {
                     // Invalid password
-                    res.send({ 'success': false });
+                    res.send({ 'error': 'Invalid password.' });
                 }
             })
-            .catch(err => {
-                res.send({ err });
-            });
+            .catch(err => res.send({ err }));
     });
 
     // Create a new user
@@ -37,7 +37,7 @@ module.exports = function (app, sequelize, models) {
             where: { email }
         })
             .then(user => {
-                
+
                 if (user) {
                     // There's already a user
                     res.send({
@@ -54,6 +54,60 @@ module.exports = function (app, sequelize, models) {
                 }
             })
             .catch(err => res.send({ err }));
+    });
+
+    // === E V E N T S === //
+
+    // Get all events
+    app.get('/events', (req, res) => {
+
+    });
+
+    // Get an event
+    app.get('/events/:id', (req, res) => {
+
+    });
+
+    // Create a new event
+    app.post('/events', (req, res) => {
+
+    });
+
+    // Update an event
+    app.post('/events/:id', (req, res) => {
+
+    });
+
+    // Delete an event
+    app.delete('/events/:id', (req, res) => {
+
+    });
+
+    // === H O B B I E S === //
+
+    // Create a hobby people can select
+    app.post('/hobbies', (req, res) => {
+
+    });
+
+    // Get all hobbies
+    app.get('/hobbies', (req, res) => {
+
+    });
+
+    // Get a specific hobby
+    app.get('/hobbies/:id', (req, res) => {
+
+    });
+
+    // Update a hobby
+    app.get('/hobbies/:id', (req, res) => {
+
+    });
+
+    // Delete a specific hobby
+    app.delete('/hobbies/:id', (req, res) => {
+
     });
 
 };
