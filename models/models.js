bafecaf9100e3908982f9d18a9707a72243a8133
Reagan <DSocketSocial@gmail.com>
2016-09-21 16:13:12 -0500

    Updates to models

--- models/models.js
+++ models/models.js
@@ -2,12 +2,16 @@ module.exports = function (sequelize) {
 
     const User = require('./User')(sequelize);
     const Event = require('./Event')(sequelize);
+    const Participant = require('./Participant')(sequelize);
+    const Hobby = require('./Hobby')(sequelize);
 
     // Define relationships here
 
     return {
         User,
-        Event
+        Event,
+        Hobby,
+        Participant
     }
 
 }
