cd1b93644599cd981a3a33f1b506227705539574
Connor Bryan <cbryan@dealersocket.com>
2016-09-21 14:28:35 -0500

    Users can no longer be signed up with a duplicate email address.

--- api/api.js
+++ api/api.js
@@ -32,13 +32,28 @@ module.exports = function (app, sequelize, models) {
         const email     = req.body.email;
         const password  = md5(req.body.password);
 
-        models.User.create({ email, password })
+        // Check to see if email is already in use
+        models.User.findOne({
+            where: { email }
+        })
             .then(user => {
-                res.send({ 'success': true, user });
+                
+                if (user) {
+                    // There's already a user
+                    res.send({
+                        'error': `A user already exists with email "${email}".`
+                    });
+
+                } else {
+
+                    models.User.create({ email, password })
+                        .then(user => {
+                            res.send({ 'success': true, user });
+                        })
+                        .catch(err => res.send({ err }));
+                }
             })
-            .catch(err => {
-                res.send({ err });
-            });
+            .catch(err => res.send({ err }));
     });
 
 };
