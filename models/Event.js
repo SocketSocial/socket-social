cb2c83041b549ee3fcb5e3b98db707dcddbf541b
Connor Bryan <cbryan@dealersocket.com>
2016-09-21 16:32:11 -0500

    Formatting

--- models/Event.js
+++ models/Event.js
@@ -5,16 +5,16 @@ const Sequelize = require('sequelize');
 module.exports = function (sequelize) {
 
   const Event = sequelize.define('event', {
-      description: {
-        type: Sequelize.STRING
-      },
-      location: {
-        type: Sequelize.STRING
-      },
-      date: {
-        type: Sequelize.DATE
-      }
-    });
+    description: {
+      type: Sequelize.STRING
+    },
+    location: {
+      type: Sequelize.STRING
+    },
+    date: {
+      type: Sequelize.DATE
+    }
+  });
 
   // Need to add association for participant
   //
