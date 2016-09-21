099f53af6446221427b5fd234dd31a27e1779bde
Connor Bryan <cbryan@dealersocket.com>
2016-09-21 17:57:52 -0500

    All front end methods for events have been added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,76 @@
 $(document).ready(() => {
-    console.log($);
+
+    function createEvent(options) {
+        return new Promise((resolve, reject) => {
+            if (!options.date || !options.location || !options.description) {
+                console.error('createEvent must be called with a date, a location, and a description.');
+                reject();
+            }
+
+            const data = {
+                date: options.date,
+                location: options.location,
+                description: options.description
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: `/events`,
+                dataType: 'JSON',
+                data,
+                success: result => resolve(result),
+                error: err => reject(err)
+            });
+        });
+    }
+
+    function getEvent(id) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: 'GET',
+                url: `/events/${id}`,
+                dataType: 'JSON',
+                success: result => resolve(result),
+                error: err => reject(err)
+            });
+        });
+    }
+
+    function getEvents() {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: 'GET',
+                url: '/events/',
+                dataType: 'JSON',
+                success: result => resolve(result),
+                error: err => reject(err)
+            });
+        });
+    }
+
+    function updateEvent(options) {
+        return new Promise((resolve, reject) => {
+            if (!options.id || !options.date || !options.location || !options.description) {
+                console.error('createEvent must be called with an id, a date, a location, and a description.');
+                reject();
+            }
+
+            const data = {
+                id: options.id,
+                date: options.date,
+                description: options.description,
+                location: options.location
+            }
+
+            $.ajax({
+                type: 'POST',
+                url: `/events/${options.id}`,
+                dataType: 'JSON',
+                data,
+                success: result => resolve(result),
+                error: err => reject(err)
+            });
+        });
+    }
+
 });
