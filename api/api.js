80850951164537b5338593ef1e22cd8bcd8b4791
Connor Bryan <cbryan@dealersocket.com>
2016-09-21 16:26:14 -0500

    Can you believe the refactoring?

--- api/api.js
+++ api/api.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const express   = require('express');
+const express = require('express');
 
-module.exports = function (app, sequelize, models) {
+module.exports = function (app, models) {
     const userApi  = require('./user.api')(app, models);
     const eventApi = require('./event.api')(app, models);
     const hobbyApi = require('./hobby.api')(app, models);
--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-// Dependencies
 const express    = require('express');
 const path       = require('path');
-const bodyParser = require('body-parser');
 
-// Database
-const sequelize = require('./config/db');
-
-// Models
-const models = require('./models/models')(sequelize);
-
-// Instantiate app
-var app = express();
-
-// Set static folder
-app.use(express.static('public'));
-app.use(bodyParser());
-
-// API
-const api = require('./api/api')(app, sequelize, models);
-
-// Routing
-const routes = require('./routes/routes')(app);
+const app         = express();
+const sequelize   = require('./config/db');
+const models      = require('./models/models')(sequelize);
+const setup       = require('./config/setup')(app);
+const api         = require('./api/api')(app, models);
+const routes      = require('./routes/routes')(app);
 
 // Start server
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
