66ced7229ec992b8d811903a485e33433cea0dba
Connor Bryan <cbryan@dealersocket.com>
2016-09-21 11:48:55 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Dependencies
 var express = require('express');
 var mysql = require('mysql');
 
+// Database
 var connection = mysql.createConnection({
   host     : '104.236.222.93',
   user     : 'root',
@@ -18,14 +20,18 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 
 connection.end();
 
+// Instantiate app
 var app = express();
 
+// Set static folder
 app.use(express.static('public'));
 
+// Routing
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// Start server
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
