2641bdbb7ca4baa843136e7149ad01ee98e2505d
Connor Bryan <cbryan@dealersocket.com>
2016-09-22 10:55:40 -0500

    Setting up for sessions

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,29 @@
 'use strict';
 
 module.exports = function (app) {
+
     app.get('/', (req, res) => {
-        res.render('../views/index');
+        res.render('../views/events');
     });
+
+    app.get('/admin', (req, res) => {
+
+        const token = req.body.token;
+
+        // if (!token || !authToken(token)) {
+        //     res.render('../views/partials/auth_error')
+        //     return false;
+        // }
+
+        res.render('../views/admin/admin');
+    });
+
 };
+
+/**
+ * Does a user have permission to access a restricted page?
+ * @param {object} token - The body's token.
+ */
+function authToken(token) {
+    return token.hasAdminAccess;
+}
