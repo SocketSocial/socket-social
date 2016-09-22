a13601ce1652733bc9aaa84c65a6f54c1437285b
Connor Bryan <cbryan@dealersocket.com>
2016-09-22 12:22:49 -0500

    Cookie session :)

--- config/setup.js
+++ config/setup.js
@@ -3,19 +3,19 @@
 const express       = require('express');
 const bodyParser    = require('body-parser');
 const cookieParser  = require('cookie-parser');
-const session       = require('express-session');
+const cookieSession = require('cookie-session');
+const Chance        = require('chance');
+
+const chance        = new Chance();
 
 module.exports = function (app) {
     app.set('view engine', 'ejs');
     app.set('trust proxy', 1);
-    app.use(session({
-        secret: 'hack2016!!',
-        cookie: {
-            test: true,
-            secure: true,
-            httpOnly: false
-        }
+    app.use(cookieParser());
+    app.use(bodyParser());
+    app.use(cookieSession({
+        name: 'session',
+        keys: ['key1', 'key2']
     }));
     app.use(express.static('public'));
-    app.use(bodyParser());
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,17 +3,10 @@
 module.exports = function (app) {
 
     app.get('/', (req, res) => {
-        console.log(req.session);
-        req.session.cookie.test = false;
-        console.log(req.session);
-        
         res.render('../views/events');
     });
 
     app.get('/admin', (req, res) => {
-        console.log(req.session);
-
-
         // if (!token || !authToken(token)) {
         //     res.render('../views/partials/auth_error')
         //     return false;
@@ -28,6 +21,6 @@ module.exports = function (app) {
  * Does a user have permission to access a restricted page?
  * @param {object} token - The body's token.
  */
-function authToken(token) {
+function checkToken(token) {
     return token.hasAdminAccess;
 }
